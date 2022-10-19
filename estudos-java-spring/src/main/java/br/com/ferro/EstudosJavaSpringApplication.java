8d53522529d05ebbe5b523118ad022948640bff1
Fabiano Ferro <fabianoferro10Qgmail.com>
2022-10-19 14:23:12 -0300

    Primeiro commit

---
+++ estudos-java-spring/src/main/java/br/com/ferro/EstudosJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosJavaSpringApplication.class, args);
+	}
+
+}
